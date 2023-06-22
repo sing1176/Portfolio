677ee0266370750a022cba083d1283f024759dd2
Sandeep Saini <sing1176@algonquinlive.com>
2023-06-22 09:45:12 -0400

    working Navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
+import Projects from './components/Projects';
 import { ThemeProvider } from './components/ThemeContext';
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <ThemeProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
+
 export default App;
---
+++ src/components/Projects.js
@@ -0,0 +1,82 @@
+import React, { useContext, useState } from 'react';
+import { motion } from 'framer-motion';
+import { Link } from 'react-router-dom';
+import ThemeButton from './ThemeButton';
+import ThemeContext from './ThemeContext';
+import BottomMenu from './BottomMenu';
+
+const Projects = () => {
+  const { theme } = useContext(ThemeContext);
+  const [mouseCoords, setMouseCoords] = useState({ x: 0, y: 0 });
+
+  const backgroundColor = theme === 'light' ? 'bg-gray-200' : 'bg-black';
+  const textColor = theme === 'light' ? 'text-black' : 'text-white';
+
+  const buttonVariants = {
+    hover: {
+      scale: 1.1,
+      transition: { duration: 0.3 },
+    },
+    pressed: {
+      scale: 0.9,
+    },
+  };
+
+  const blobDesign = {
+    background: 'linear-gradient(to right, pink, purple)',
+    height: '200px',
+    width: '200px',
+    filter: 'blur(80px)',
+  };
+
+  const onMouseMove = (e) => {
+    setMouseCoords({ x: e.clientX, y: e.clientY });
+  };
+
+  return (
+    <>
+      <div
+        className={
+          "font-['Poppins'] flex min-h-full w-screen items-center overflow-hidden z-20 " +
+          backgroundColor
+        }>
+        {/* Navigation and Theme Button */}
+        <div className="absolute bottom-4 right-4 md:top-4 md:right-4 z-40">
+          <div className="flex flex-row">
+            <Link to="/">
+              <motion.button
+                variants={buttonVariants}
+                whileHover="hover"
+                whileTap="pressed"
+                className={`text-white font-bold py-2 px-4 rounded ml-2 ${backgroundColor}`}>
+                Go to Home
+              </motion.button>
+            </Link>
+            <ThemeButton />
+          </div>
+        </div>
+        {/* Projects */}
+      </div>
+      {/* Bottom Menu */}
+      {/* <BottomMenu /> */}
+      {/* Blob */}
+      {/* <motion.div
+        animate={{
+          x: mouseCoords.x,
+          y: mouseCoords.y,
+          rotate: 360,
+          scale: [1, 1.4, 1],
+        }}
+        transition={{
+          duration: 5,
+          ease: 'circOut',
+          repeat: Infinity,
+        }}
+        className="fixed top-0 left-0 overflow-hidden"
+        style={ blobDesign }>
+      </motion.div> */}
+    </>
+  );
+};
+
+export default Projects;
